bfee18696e52f54203c3d4b8e05fb0eb9b0a9eed
btdev0306 <btdev0306@outlook.com>
2024-05-14 11:30:11 -0400

    update

--- hello-world/programs/hello-world/src/lib.rs
+++ hello-world/programs/hello-world/src/lib.rs
@@ -7,6 +7,7 @@ pub mod hello_world {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Hello world, from solana smart contract");
         Ok(())
     }
 }
