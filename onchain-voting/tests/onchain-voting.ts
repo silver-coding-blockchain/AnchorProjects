f1cb294af5596684c19ef7e0e6c2e076ff2f414f
btdev0306 <btdev0306@outlook.live.com>
2024-05-18 04:44:32 -0500

    update

--- onchain-voting/tests/onchain-voting.ts
+++ onchain-voting/tests/onchain-voting.ts
@@ -7,10 +7,45 @@ describe("onchain-voting", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.OnchainVoting as Program<OnchainVoting>;
+  let voteBank = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Creating vote bank for public to vote", async () => {
+    const tx = await program.methods.initVoteBank()
+      .accounts({
+        voteAccount: voteBank.publicKey,
+      })
+      .signers([voteBank])
+      .rpc();
+    console.log("TxHash ::", tx);
+  });
+
+
+  it("Vote for GM", async () => { 
+    const tx = await program.methods.gibVote({gm:{}})
+    .accounts({
+      voteAccount: voteBank.publicKey,
+    })
+    .rpc();
+    console.log("TxHash ::", tx);
+
+
+    let voteBankData = await program.account.voteBank.fetch(voteBank.publicKey);
+    console.log(`Total GMs :: ${voteBankData.gm}`)
+    console.log(`Total GNs :: ${voteBankData.gn}`)
+  });
+
+
+  it("Vote for GN", async () => { 
+    const tx = await program.methods.gibVote({gn:{}})
+    .accounts({
+      voteAccount: voteBank.publicKey,
+    })
+    .rpc();
+    console.log("TxHash ::", tx);
+
+
+    let voteBankData = await program.account.voteBank.fetch(voteBank.publicKey);
+    console.log(`Total GMs :: ${voteBankData.gm}`)
+    console.log(`Total GNs :: ${voteBankData.gn}`)
   });
 });
