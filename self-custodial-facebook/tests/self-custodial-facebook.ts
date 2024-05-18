7507bedd64697497e9fa2fd35101e53e19445f78
btdev0306 <btdev0306@outlook.live.com>
2024-05-18 07:45:21 -0500

    update

--- self-custodial-facebook/tests/self-custodial-facebook.ts
+++ self-custodial-facebook/tests/self-custodial-facebook.ts
@@ -3,14 +3,68 @@ import { Program } from "@coral-xyz/anchor";
 import { SelfCustodialFacebook } from "../target/types/self_custodial_facebook";
 
 describe("self-custodial-facebook", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider =  anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.SelfCustodialFacebook as Program<SelfCustodialFacebook>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+
+  it("Creating a new account for user", async () => {
+    const ix = await program.methods.createFacebook(1,"Deep", "always tinkring", "0xdeep")
+    const userFacebookAddress = (await ix.pubkeys()).facebookAccount
+    console.log("User facebook address :: ", userFacebookAddress.toString());
+    // Create user's facebook address
+    const tx = await ix.rpc()
     console.log("Your transaction signature", tx);
+    // User Details
+    let userDetails = await program.account.facebookAccount.fetch(userFacebookAddress);
+    console.log(`Created a new account with following details \n Name :: ${userDetails.name} \n Status :: ${userDetails.status} \n Twitter :: ${userDetails.twitter}\n Bump::${userDetails.bump}`)
+  });
+
+
+  it("Update My Status", async () => { 
+    const ix = await program.methods.updateStatus("&mut self :crab")
+    const userFacebookAddress = (await ix.pubkeys()).facebookAccount
+    console.log("usrFaceBook Address :: ", userFacebookAddress.toString());
+    // Create user's facebook address
+    const tx = await ix.rpc()
+    console.log("Your transaction signature", tx);
+    // User Details
+    let userDetails = await program.account.facebookAccount.fetch(userFacebookAddress);
+    console.log(`Created a new account with following details \n Name :: ${userDetails.name} \n Status :: ${userDetails.status} \n Twitter :: ${userDetails.twitter}`)
+  });
+  it("Find Someone's Facebook", async () => {
+    const userAddress = new anchor.web3.PublicKey("Gz2k7789kKnoeDo9TWXpCmSudp5DW22u8FvnRcFS5aS6");
+    const [userFacebookAccount, _] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('self-custodial-facebook2'),
+        userAddress.toBuffer(),
+      ],
+      program.programId
+    )
+    try {
+      let userDetails = await program.account.facebookAccount.fetch(userFacebookAccount);
+      console.log(
+        `Created a new account with following details \n Name :: ${userDetails.name} \n Status :: ${userDetails.status} \n Twitter :: ${userDetails.twitter}`
+      )
+    } catch (error) {
+      console.log("Users account does not exist :: ", error) 
+    }
+  });
+
+
+  it("Close My Facebook Account", async () => {
+    const ix = await program.methods.deleteAccount()
+    const userFacebookAddress = (await ix.pubkeys()).facebookAccount
+    console.log("user facebook address :: ", userFacebookAddress.toString());  
+    // Create user's facebook address
+    const tx = await ix.rpc()
+    console.log("Your transaction signature", tx);
+    // User Details Not found, 'cuz we closed the account
+    try {
+      let userDetails = await program.account.facebookAccount.fetch(userFacebookAddress);
+      console.log(`Created a new account with following details \n Name :: ${userDetails.name} \n Status :: ${userDetails.status} \n Twitter :: ${userDetails.twitter}`)
+    } catch {
+      console.log("User Details Not found, 'cuz we close the account");
+    }
   });
 });
